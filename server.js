d723d9f80d680474e90b2be8008924528d5025e3
Christopher Chen <chris.chen@yale.edu>
2017-10-05 05:40:19 +0000

    Added Response

--- server.js
+++ server.js
@@ -8,4 +8,7 @@ app.get('/', (request, response) => {
     response.send('Hello MGT-656!');
 });
 
+app.get('/comfortable-crocodile', (request, response) => {
+    response.send('Hello MGT-656! You have reached comfortable crocodile');
+});
 app.listen(process.env.PORT || 4000);
